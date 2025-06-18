97d2bf3f4dc66198f4f368c8ac55134f9f50c784
komame <h.komame.sys24@morijyobi.ac.jp>
2025-06-18 12:58:08 +0900

    ガチャができるようにする
    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { useState } from 'react'
+import Result from './components/Result'
+import Button from './components/Button'
+
 function App() {
+  const [result, setResult] = useState({"result": "X"})
+
+  const handleClick = async () => {
+      const response = await fetch('http://localhost:3000/gacha')
+      const data = await response.json()
+      setResult(data)
+  };
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Result result={result} />
+      <Button handleClick={handleClick}/>
+    </div>
   )
 }
 
